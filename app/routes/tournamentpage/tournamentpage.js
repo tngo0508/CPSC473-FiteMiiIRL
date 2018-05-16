86e22c8f150f88c305df4d0028c9927255a684f7
Brian <ballerbrian4@gmail.com>
2018-05-15 22:24:08 -0700

    updated positioning of tournament details in page

--- app/routes/tournamentpage/tournamentpage.js
+++ app/routes/tournamentpage/tournamentpage.js
@@ -4,14 +4,9 @@ import {inject as service} from '@ember/service';
 export default Route.extend({
   maps: service(),
 
-
-
   model(params){
     return this.store.findRecord('tournament', params.tournament_id);
   }
-<<<<<<< HEAD
 
 
-=======
->>>>>>> caea6e1949c72f1d14d4ec1ba6e794c5699908ac
 });
